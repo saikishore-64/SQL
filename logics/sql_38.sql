0a7fde68332141decc84115cc0fb6b7caed4855f
Sai Kishore G <98144845+saikishore-64@users.noreply.github.com>
2025-08-13 22:34:13 +0530

    Create sql_38.sql

---
+++ logics/sql_38.sql
@@ -0,0 +1,3 @@
+38) write a SQL query to find out the callers whose first and last call is with the same person on agiven day
+columns: callerid -- who called, recipientid -- who recieved call
+Sol:
